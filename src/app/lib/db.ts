7f10df470791e91c08b5e6afff416000ff47d148
Ayan <ayanhasnain2572006@gmail.com>
2024-12-09 16:04:45 +0530

    changes

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -7,11 +7,7 @@ const connectDB = async () => {
   }
 
   try {
-<<<<<<< HEAD
     await mongoose.connect(process.env.MONGO_URI as string);
-=======
-    await mongoose.connect("mongodb+srv://ayanhasnain2572006:8797131193@practicemern.lebin.mongodb.net/diziimpact");
->>>>>>> cf845455e425bebe8e3e720ffa5ee6fe73845558
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
